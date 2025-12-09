55243cc039a37b33f88af4f76e353ab3800730c8
r-keenan <ross.keenan@protonmail.com>
2025-12-09 10:36:38 -0600

    added react/tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-dark-blue/theme.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+    <QueryClientProvider client={queryClient}>
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
